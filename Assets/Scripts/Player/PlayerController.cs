300b07007b4d58539c8369fa06e2b3b261c00e49
Antonio Sarmiento <antoniosaryon155@gmail.com>
2023-06-23 11:57:37 -0500

    Porsiacaso

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
-using UnityEditor.SceneManagement;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using System;
